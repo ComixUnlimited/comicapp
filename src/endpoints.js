2887a3d83cb93d8410bccdecc763f2e3e2ffe9ee
elijah . <elijahseifullah@gmail.com>
2025-02-07 04:55:41 -0500

    vercel proxy

--- src/endpoints.js
+++ src/endpoints.js
@@ -6,14 +6,14 @@ exports.URL = {
 };
 
 exports.DC = {
-    dcHome: "http://localhost:4000/comics/cat/dc/page",
+    dcHome: "https://comixunlimited.vercel.app/comics/cat/dc/page",
     dcWeek: "http://localhost:4000/comics/tag/dc-week/page",
     dcVertigo: "http://localhost:4000/comics/tag/vertigo/page",
     dcWildstorm: "http://localhost:4000/comics/tag/wildstorm/page"
 };
 
 exports.Marvel = {
-    marvelHome: "http://comixunlimited.vercel.app/comics/cat/marvel/page"
+    marvelHome: "https://comixunlimited.vercel.app/comics/cat/marvel/page"
 };
 
 exports.otherComics = {
