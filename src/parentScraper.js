484467c3d3a44d0cda281f1735dcb136c8ee2e50
elijah . <elijahseifullah@gmail.com>
2025-02-07 05:19:42 -0500

    vercel proxy

--- src/parentScraper.js
+++ src/parentScraper.js
@@ -1,59 +1,70 @@
-const cheerio = require('cheerio');
-const axios = require('axios');
+import { load } from 'cheerio';
+import axios from 'axios';
+import infoScraper from './infoScraper';
 
-const { infoScraper } = require('./infoScraper');
-
-exports.parentScraper = (uri, page) => {
-
+export const parentScraper = (uri, page) => {
   return new Promise((resolve, reject) => {
+    const targetUrl = `${uri}/${page}`;  // Construct the URL for the page to scrape
 
-      axios(`${uri}/${page}`).then(response => {
+    axios(targetUrl)
+      .then(response => {
+        const $ = load(response.data);
+        const comics = [];
 
-          const $ = cheerio.load(response.data);
-          const comics = [];
+        $('article').each(function() {
+          let coverPage = $(this).find('img').attr('src');
+          let valid = $(this).find('.post-info').children().remove().end().text();
+          let href = $(this).find('a').attr('href');
 
-          $('article').each(
-              function() {
+          // If the href is a full URL (https://getcomics.org/...), remove the base part
+          if (href.startsWith('https://getcomics.org')) {
+            // Remove 'https://getcomics.org' and replace with the proxy URL base
+            href = href.replace('https://getcomics.org', 'https://getcomics.info');
+          }
 
-                  let coverPage = $(this).find('img').attr('src');
-                  let valid = $(this).find('.post-info').children().remove().end().text();
-                  let href = $(this).find('a').attr('href');
+          // Only include individual comics, not bundles
+          if (valid) {
+            const promise = new Promise((resolve, reject) => {
+              axios(href)
+                .then(response => {
+                  const $ = load(response.data);
 
-                  //only include individual comics not the bundles
-                  if(valid) {
-                      const promise = new Promise((resolve, reject) => {
-                          axios(`${href}`).then(response => {
+                  const title = $('.post-info').find('h1').text().trim();
+                  const description = $('.post-contents').find('p').first().children().remove().end().text().trim();
+                  const scrapedInfo = $('.post-contents > p:nth-child(7)').text().split("|").splice(1, 3).join().toString();
 
-                              const $ = cheerio.load(response.data);
+                  let downloadLinks = {};
+                  $('.aio-pulse').each(function() {
+                    const scrapedDownloadTitle = $(this).children('a').attr('title').split(' ').join('').toLocaleUpperCase();
+                    const scrapedDownloadLinks = $(this).children('a').attr('href');
+                    downloadLinks[scrapedDownloadTitle] = scrapedDownloadLinks;
+                  });
 
-                              const title = $('.post-info').find('h1').text().trim();
-                              const description = $('.post-contents').find('p').first().children().remove().end().text().trim();
-                              const scrapedInfo = $('.post-contents > p:nth-child(7)').text().split("|").splice(1, 3).join().toString();
+                  const information = infoScraper(scrapedInfo);
 
-                              let downloadLinks = {};
-                              $('.aio-pulse').each(function() {
-                                  const scrapedDownloadTitle = $(this).children('a').attr('title').split(' ').join('').toLocaleUpperCase();
-                                  const scrapedDownloadLinks = $(this).children('a').attr('href');
-                                  downloadLinks[ scrapedDownloadTitle ] = scrapedDownloadLinks;
-                              });
+                  const comic = {
+                    title,
+                    coverPage,
+                    description,
+                    information,
+                    downloadLinks,
+                  };
 
-                              const information = infoScraper(scrapedInfo);
+                  resolve(comic);
+                })
+                .catch(err => {
+                  reject(err);
+                });
+            });
 
-                              const comic = {
-                                  title, coverPage, description, information, downloadLinks
-                              };
-                              resolve(comic);
-                          }).catch(
-                              err => {
-                                  if(err) { reject(err); }
-                              });
-                      });
-                      comics.push(promise);
-                  }
-              });
-          resolve(Promise.all(comics));
-      }).catch(err => {
-          if(err) { reject(err); }
+            comics.push(promise);
+          }
+        });
+
+        resolve(Promise.all(comics));
+      })
+      .catch(err => {
+        reject(err);
       });
   });
 };
